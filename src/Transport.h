9a570531945d231165ae134f5ba86817acdc0c84
Katya D <your_email@hotabsc.com>
2017-01-17 19:18:31 +0400

    sdal kursovuyu

--- src/Transport.h
+++ src/Transport.h
@@ -18,18 +18,6 @@ class Transport{
         virtual void initElement();
         virtual void getParameters();
         virtual void introduce();
-        virtual void setWheels(string s){}
-        virtual string getWheels(){return 0;}
-        virtual void setDisplacement(string s){}
-		virtual string getDisplacement(){return 0;}
-		virtual void setRailCanvasWidth(string s){}
-		virtual string getRailCanvasWidth(){return 0;}
-		virtual void setClearance(string s){}
-        virtual string getClearance(){return 0;}
-        virtual void setDeckSqare(string s){}
-        virtual string getDeckSqare(){return 0;}
-        virtual void setImmersionDepth(string s){}
-        virtual string getImmersionDepth(){return 0;}
     protected:
     private:
         string passengers;
